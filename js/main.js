1f92f09dadff158523f9fd2e6e6c57a23ea34568
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-11-02 16:06:57 +0100

    secondo fatto

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 
-// JSnack 2
-// Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
-// Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
-// Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
-// JSnack 3
-// Creiamo un array di oggetti (scelti da voi)
-// Creiamo una copia da questo array di partenza e aggiungiamo ai singoli elementi una nuova proprietà position che contiene una lettera casuale.
-// Non dobbiamo modificare l'array iniziale.
-
-
 $(document).ready(function(){
 // JSnack 1
 // Creare un array di oggetti
@@ -51,7 +41,26 @@ var loadin={
 var html=template(loadin);
 $('.container').append(html)
 
+// JSnack 2
+// Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
+// Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
+// Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
+var nomi=['luigi', 'marco', 'pippo', 'massimo'];
+var tot=nomi.length-1;
+var Narray=[]
+var domUtente=parseInt(prompt('inserisici 2 numeri '))
+var domUtente2=parseInt(prompt('inserisici 2 numeri '))
+for(var i=0; i<nomi.length; i++){
+    if (domUtente<=i && domUtente2>=i){
+        Narray.push(nomi[i])
+    }
+}
+console.log(nomi)
+console.log(Narray)
 
-
+// JSnack 3
+// Creiamo un array di oggetti (scelti da voi)
+// Creiamo una copia da questo array di partenza e aggiungiamo ai singoli elementi una nuova proprietà position che contiene una lettera casuale.
+// Non dobbiamo modificare l'array iniziale.
 
 }) //fine READY----------------------------
