9e08e1cf8a5b0dac0a8002a50955f0ea8b5ea9b4
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-11-02 15:49:06 +0100

    non riesco a capire il eprchè non stampa tutto il cod su html?!?!?

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
 
-console.log('okkokokok')
+// JSnack 2
+// Partendo da un array di nomi (scelti da voi) creiamo un nuovo array.
+// Chiedere due numeri all'utente per definire la posizione iniziale e finale dal quale estrarre i valori dall'array originale.
+// Il nuovo array deve contenere gli elementi dell'array originale aventi la posizione (indice array)  compresa tra i due numeri inseriti dall'utente.
+// JSnack 3
+// Creiamo un array di oggetti (scelti da voi)
+// Creiamo una copia da questo array di partenza e aggiungiamo ai singoli elementi una nuova proprietà position che contiene una lettera casuale.
+// Non dobbiamo modificare l'array iniziale.
+
 
 $(document).ready(function(){
+// JSnack 1
+// Creare un array di oggetti
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore con handlebars.
+var biciclette=[
+    {
+        nome:'pippo',
+        perso:10
+    },
+    {
+        nome:'nicola',
+        perso:33
+    },
+    {
+        nome:'luca',
+        perso:15
+    },
+];
+
+// handelbars----------
+var source=$('#bike-B').html();
+var template = Handlebars.compile(source);
 
 
+var confrontoBici=biciclette[0];
 
+for(var i=1; i<biciclette.length; i++){
+    if(biciclette[i].peso<confrontoBici.peso){
+    confrontoBici=biciclette[i]
+    }
+}
+console.log(confrontoBici)
+var loadin={
+    nome:confrontoBici.nome,
+    peso:confrontoBici.peso,
 
+}
 
-
+var html=template(loadin);
+$('.container').append(html)
 
 
 
